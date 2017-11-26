a1b28ee2cf28bb9fb8737c346141d4453bbdccfd
Mishio595 <tenseiken595@gmail.com>
2017-11-25 21:05:14 -0700

    Updating luvit-reql

--- deps/luvit-reql/init.lua
+++ deps/luvit-reql/init.lua
@@ -1,6 +1,7 @@
 local connect = require('connect')
 local reql = require('Utils/reql.lua')
 local logger = require('Utils/logger.lua')
+local emitter = require('Utils/emitty.lua')
 
 -- Default options table || Do NOT edit, pass your options to the function instead
 
@@ -21,8 +22,8 @@ return {
 	connect = function(options)
 		options = options and options or {}
 		local type = type(options)
-		if not type == 'table' then
-			return error(format('Bad argument #1 to luvit-reql/init.lua, table expected, got %s', type))
+		if type ~= 'table' then
+			return error(format('Bad argument #1 to luvit-reql.connect(), table expected, got %s', type))
 		end
 		for k,v in pairs(default) do
 			if not options[k] then
@@ -33,7 +34,7 @@ return {
 			options.address = options.address:sub(1,#options.address-1)
 		end
 		if not find(options.address, 'https?', 1) == 1 then
-			logger.warn.format('Procotol not supplied, defaulting to http://')
+			logger.warn('Procotol not supplied, defaulting to http://')
 			options.address = format('http://%s', options.address)
 		end
 		return connect(options)
@@ -41,5 +42,9 @@ return {
 
 	reql = function()
 		return reql()
-	end
+	end,
+	
+	emitter = emitter,
+	
+	logger = logger
 }
