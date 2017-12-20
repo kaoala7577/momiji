355693a8053bbf7e13c1341fd567278ed3218d5a
Mishio595 <tenseiken595@gmail.com>
2017-12-19 21:05:52 -0700

    even more for logging...

--- Modules/Database.lua
+++ Modules/Database.lua
@@ -98,7 +98,7 @@ function Database:update(guild,index,value) --luacheck: ignore self
 			Database.cache[id].id=id
 		end
 		local data,err,edata=Database._conn.reql().db('momiji').table('guilds').inOrUp(Database.cache[id]).run()
-		logger:log(err and 1 or 4, "%sDATA:\n%s", err and "ERROR:\n"..err.."" or "", json.encode(data))
+		logger:log(err and 1 or 4, "GUILD: %s INDEX: %s\n%sDATA:\n%s", id, index, err and "ERROR:\n"..err.."\n" or "", json.encode(data))
 		if err then
 			print('UPDATE')
 			print(err)
