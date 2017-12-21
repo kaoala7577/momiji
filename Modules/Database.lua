0ce110f4613329cf774f6c5d33f36b9d891bff57
Mishio595 <tenseiken595@gmail.com>
2017-12-20 22:10:38 -0700

    wtaf am i doing

--- Modules/Database.lua
+++ Modules/Database.lua
@@ -99,7 +99,7 @@ function Database:update(guild,index,value) --luacheck: ignore self
 		if not Database.cache[id].id then
 			Database.cache[id].id=id
 		end
-		local data,err,edata=Database._conn.reql().db('momiji').table('guilds').inOrUp(Database.cache[id]).run()
+		local data,err,edata=Database._conn.reql().db('momiji').table('guilds').inOrRe(Database.cache[id]).run()
 		logger:log(err and 1 or 4, "GUILD: %s INDEX: %s\n%sDATA:\n%s", id, index, err and "ERROR:\n"..err.."\n" or "", json.encode(data))
 		if err then
 			print('UPDATE')
