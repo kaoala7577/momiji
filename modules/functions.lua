488aff69213a4a4271ec05bc3ad32d3fcc226679
Mishio595 <tenseiken595@gmail.com>
2018-03-14 07:41:04 -0600

    ugh

--- modules/functions.lua
+++ modules/functions.lua
@@ -92,7 +92,7 @@ function functions.prettyTime(t)
 	local out = ""
 	for k,v in pairsByKeys(order) do
 		if t[k] then
-			t[k] = t[k]-1
+			t[k] = t[k]>0 and t[k]-1 or t[k]
 			if t[k]==1 then
 				out = out~="" and out..", "..t[k].." "..v or t[k].." "..v
 			elseif t[k]~=0 then
